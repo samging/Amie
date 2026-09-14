774dfa4cc43bf40f80963256059663d4dcc82b0c
/sam <48388652+samging@users.noreply.github.com>
2026-09-15 00:42:49 +0200

    env update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 export JAVA_HOME=/Users/samuel/Library/Java/JavaVirtualMachines/ms-21.0.10/Contents/Home
-export GITHUB_TOKEN=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
+export GITHUB_TOKEN=
 
 echo "Starting Amie Full Stack..."
 
